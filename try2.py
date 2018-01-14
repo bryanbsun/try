3f94b719e4585c9edcc1634502d28aa0df3258ae
Bryan <bryanbsun@gmail.com>
2018-01-14 23:18:46 +0800

    Initial commit

---
+++ try2.py
@@ -0,0 +1,15 @@
+cliches = {
+	"Moe":		"A wise guy, huh?",
+	"Larry":	"Ow!",
+	"Curly":	"Nyuk nyuk!"
+}
+
+stooge = 1
+print (cliches)
+
+
+
+
+
+
+
---
+++ try4.py
@@ -0,0 +1,4 @@
+language = 7
+print("language %s: I am Python.  What's for supper?" %language)
+
+
